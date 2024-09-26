1d8909bc583bf6638b48dcfe7ff9b8eae43b31ea
humhliao <humhliao@gmail.com>
2024-09-26 21:30:57 +0700

    3 ex-so

---
+++ javascript/03-strings/ex-so/index.js
@@ -0,0 +1,18 @@
+// console.log('My name is:');
+// console.log('Hoang');
+// console.log('My name is: ' + 'Hoang');
+// console.log('Total cost: $' + (3 + 5));
+// console.log(`Total cost: $${5 + 3}`);
+// alert(`Total cost: $${3 + 5}`);
+
+console.log('Total cost: $' + (599 + 295) / 100);
+console.log(`Total cost: $${(599 + 295) / 100}`);
+
+//alert(`Total cost: $${(599 + 295) / 100}`);
+// alert(`Total cost: $${(599 + 295) / 100}
+// Thank you, come again!`);
+
+console.log(`Items (${2 + 2}): $${(2* 2095 + 2 * 799) / 100}`);
+console.log(`Shipping & handling: $${(499 + 499) / 100}`);
+console.log(`Total before tax: ${(2095 * 2 + 2 * 799 + 499 * 2) / 100}`);
+console.log(`Estimated tax (10%): $${Math.round((2095 * 2 + 2 * 799 + 499 * 2 ) * 0.1) / 100}`);
