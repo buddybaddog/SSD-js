0bec719c86ef2674cf4a5c3c6a0db86cd4975c1f
humhliao <humhliao@gmail.com>
2024-09-26 20:24:54 +0700

    2 ex-so

---
+++ javascript/02-numbers-and-math/ex-so/index.js
@@ -0,0 +1,15 @@
+console.log(10 + 3 * 8 + 5);
+console.log((1 + 3 * 8 + 5) / 3);
+console.log((1850 + 2 * 750) / 100);
+console.log((18.50 + 2 * 7.50) * 0.1);
+console.log((18.50 + 2 * 7.50) * 0.2);
+
+console.log((1899 + 799 + 2095) / 100);
+console.log(Math.round((1899 + 799 + 2095 + 499)) / 100);
+console.log(Math.round((1899 + 799 + 2095 + 499) * 0.1) / 100 + Math.round((1899 + 799 + 2095 + 499)) / 100);
+console.log(Math.floor(2.8));
+console.log(Math.ceil(2.2));
+
+console.log((25 * 9/5) + 32);
+console.log((86 - 32) * 5/9);
+console.log((-5 * 9 / 5) + 32);
