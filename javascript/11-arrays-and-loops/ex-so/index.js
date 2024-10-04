6be91874fbc5309872cf816fbec020c4d216c14d
humhliao <humhliao@gmail.com>
2024-10-04 21:12:18 +0700

    11 ex-so

---
+++ javascript/11-arrays-and-loops/ex-so/index.js
@@ -0,0 +1,158 @@
+/*
+const nums= [10, 20, 30];
+
+nums[2] = 99;
+console.log(nums);
+
+nums[nums.length - 1] = 9;
+console.log(nums);
+
+function getLastValue(array) {
+  lastIndex = array[array.length - 1];
+  return lastIndex;
+}
+
+console.log(getLastValue([1, 20, 22, 24, 5]));
+console.log(getLastValue(['hi', 'hello', 'good']));
+
+function arraySwap(array) {
+  const firstValue = array[0];
+  const lastValue = array[array.length - 1];
+
+  array[0] = lastValue;
+  array[array.length - 1] = firstValue;
+  return array;
+}
+
+console.log(arraySwap([1, 20, 22, 24, 5]));
+console.log(arraySwap(['hi', 'hello', 'good']));
+
+for(let i = 0;i <= 10; i += 2) {
+  console.log(i);
+}
+
+for(let i = 5;i >= 0; i--) {
+  console.log(i);
+}
+
+let i = 0;
+
+while (i <= 10) {
+  console.log(i);
+  i += 2;
+}
+
+let x = 5;
+
+while (x >= 0) {
+  console.log(x);
+  x--;
+}
+
+const myArray = [1, 2, 3];
+const result = [];
+
+for(let i = 0;i < myArray.length; i++) {
+  result.push(myArray[i] + 1);
+}
+
+console.log(result);
+
+
+function addOne(array) {
+  const result = [];
+  for(let i = 0;i < array.length; i++) {
+    result.push(array[i] + 1);
+  }
+  return result;
+}
+
+console.log(addOne([1, 2, 3]));
+console.log(addOne([-2, -1, 0, 99]));
+
+
+function addArrays(array, num) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[i] + num);
+
+  }
+  return result;
+}
+
+console.log(addArrays([1, 2, 3], 2));
+console.log(addArrays([1, 2, 3], 3));
+
+
+
+function addArrays(array1, array2) {
+  const result = [];
+  for(let i = 0;i < array1.length; i++) {
+    result.push(array1[i] + array2[i]);
+  }
+
+  return result;
+}
+
+console.log(addArrays([1, 1, 2], [1, 1, 3]));
+console.log(addArrays([1, 2, 3], [4, 5, 6]));
+
+
+function countPositive(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    
+    if (nums[i] > 0) {
+      count++;
+    }
+    // return count; 
+  }
+
+  return count; 
+}
+
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([-2, 3, -5, 7, 10]));
+*/
+
+function minMax(nums) {
+  let min = null;
+  let max = null;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < min || min === null) {
+      min = nums[i];
+    } 
+    if (nums[i] > max || max === null) {
+      max = nums[i];
+    }
+  }
+  return {'min': min, 'max': max};
+}
+
+console.log(minMax([1, -3, 5]));
+console.log(minMax([-2, 3, -5, 7, 10]));
+
+console.log(minMax([]));
+console.log(minMax([3]));
+
+function countWords(words) {
+  const result = {};
+
+  for (let i = 0; i < words.length; i++) {
+    
+    // result[word] adds/accesses a property using whatever is
+    // saved inside the 'word' variable.
+    // If word = 'apple', result[word] will do result['apple']
+    // If word = 'grape', result[word] will do result['grape']
+    const word = words[i];
+    if(!result[word]) {
+      result[word] = 1;
+    } else {
+      result[word]++;
+    }
+  }
+  
+  return result;
+}
+
+console.log(countWords(['apple', 'grape', 'apple', 'apple']));
