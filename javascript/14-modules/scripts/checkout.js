811818c4fd307fc818333032ec10a2e0f2a505fd
humhliao <humhliao@gmail.com>
2024-10-19 10:26:05 +0700

    Fix the cart home page and reuse function by modules

--- javascript/14-modules/scripts/checkout.js
+++ javascript/14-modules/scripts/checkout.js
@@ -1,4 +1,6 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart,
+  calculateCartQuantity
+} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -108,14 +110,16 @@ document.querySelectorAll('.js-delete-link')
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+
+      updateCartQuantity();
     });
   });
 
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+function updateCartQuantity() {
+const cartQuantity = calculateCartQuantity();
 
   document.querySelector('.js-return-to-home-link')
     .innerHTML = `${cartQuantity} items`;
+}
+
+updateCartQuantity();
