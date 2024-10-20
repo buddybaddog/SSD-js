2ad509adbbb3fb8784d384950aa17331af2f8a50
humhliao <humhliao@gmail.com>
2024-10-20 12:11:47 +0700

    Up-to-date the cart quantity use MVC

--- javascript/15-external-libraries/scripts/checkout/orderSummary.js
+++ javascript/15-external-libraries/scripts/checkout/orderSummary.js
@@ -5,6 +5,7 @@ import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -119,6 +120,7 @@ export function renderOrderSummary() {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
+        renderCheckoutHeader();
         renderOrderSummary();
         renderPaymentSummary();
       });
@@ -129,8 +131,9 @@ export function renderOrderSummary() {
         element.addEventListener('click', () => {
           const {productId, deliveryOptionId} = element.dataset;
           updateDeliveryOption(productId, deliveryOptionId);
+
+
           renderOrderSummary();
-          // renderPaymentSummary();
           renderPaymentSummary();
         });
       });
