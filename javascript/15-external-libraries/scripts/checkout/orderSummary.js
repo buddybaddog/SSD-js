eed3e82c542eb8f5a5d2ff91a50f7f7d46fd27bc
humhliao <humhliao@gmail.com>
2024-10-20 11:45:56 +0700

    Make number of payment summary show the actual

--- javascript/15-external-libraries/scripts/checkout/orderSummary.js
+++ javascript/15-external-libraries/scripts/checkout/orderSummary.js
@@ -119,12 +119,7 @@ export function renderOrderSummary() {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-        container.remove();
-
-        // renderPaymentSummary();
+        renderOrderSummary();
         renderPaymentSummary();
       });
     });
